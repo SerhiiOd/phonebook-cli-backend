fe6c1c794a548687a40ceb5c437efbc274e2f21e
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-09-08 22:32:48 +0300

    upd-code

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,19 +1,20 @@
-const User = require('../db/models/userModel');
+const User = require("../db/models/userModel");
 const { SECRET_KEY } = process.env;
-const gravatar = require('gravatar');
-const jwt = require('jsonwebtoken');
-const fs = require("fs/promises")
-const path = require("path")
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const fs = require("fs/promises");
+const path = require("path");
 
-const avatarsDir = path.join(__dirname, "../", "public", "avatars")
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const signupController = async (req, res) => {
+  console.log(req.body);
   const { name, email, password } = req.body;
 
   const user = await User.findOne({ email });
 
   if (user) {
-    res.status(409).json({ message: 'Message email in use' });
+    res.status(409).json({ message: "Message email in use" });
     return;
   }
 
@@ -48,14 +49,14 @@ const loginController = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    res.status(401).json({ message: 'Email or password invalid' });
+    res.status(401).json({ message: "Email or password invalid" });
     return;
   }
 
   const compare = await user.comparePassword(password);
 
   if (!compare) {
-    res.status(401).json({ message: 'Email or password invalid' });
+    res.status(401).json({ message: "Email or password invalid" });
     return;
   }
 
@@ -75,7 +76,7 @@ const loginController = async (req, res) => {
 
 const logoutControllers = async (req, res) => {
   const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: '' });
+  await User.findByIdAndUpdate(_id, { token: "" });
   res.status(204).send();
 };
 
@@ -84,15 +85,15 @@ const currentControllers = (req, res) => {
   res.status(200).json({ email, name, avatar });
 };
 
-const updateAvatar = async(req, res) => {
+const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-  const fileName = `${_id}_${originalname}`
-  const resultUpload = path.join(avatarsDir, fileName)
-  await fs.rename(tempUpload, resultUpload)
-  const avatarURL = path.join('avatar', fileName) 
+  const fileName = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, fileName);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatar", fileName);
   await User.findByIdAndUpdate(_id, { avatar: avatarURL });
-  res.status(200).json({avatar: avatarURL})
+  res.status(200).json({ avatar: avatarURL });
 };
 
 module.exports = {
